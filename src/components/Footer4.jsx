8a613a02d573c0dbf6630771fa15fd2108478ce3
lyrianapps <lyrianapps@gmail.com>
2025-07-14 13:25:06 -0400

    feat: update

--- src/components/Footer4.jsx
+++ src/components/Footer4.jsx
@@ -15,11 +15,7 @@ export function Footer4() {
       <div className="container">
         <div className="grid grid-cols-1 items-center justify-center justify-items-center gap-x-[4vw] gap-y-12 pb-12 md:pb-18 lg:grid-cols-[0.25fr_1fr_0.25fr] lg:justify-between lg:gap-y-4 lg:pb-20">
           <a href="#" className="lg:justify-self-start">
-            <img
-              src="https://d22po4pjz3o32e.cloudfront.net/logo-image.svg"
-              alt="Logo image"
-              className="inline-block"
-            />
+            Sample Landscaping
           </a>
           <ul className="grid grid-flow-row grid-cols-1 items-start justify-center justify-items-center gap-6 md:grid-flow-col md:grid-cols-[max-content] md:justify-center md:justify-items-start">
             <li className="font-semibold">
